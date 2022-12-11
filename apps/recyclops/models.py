77d66d1a39d7fa458c9925b4ab3ddfd21e8c1faf
philDaprogrammer <prstanto@buffalo.edu>
2022-12-11 13:08:14 -0500

    everything is up to date

--- apps/recyclops/models.py
+++ apps/recyclops/models.py
@@ -18,6 +18,16 @@ need to be declared as class variables.
 """
 
 class centers(db.Model):
+    row_num = sa.Column(sa.Integer, primary_key=True)
+    name    = sa.Column(sa.String)
+    mat     = sa.Column(sa.String)
+
+    def __init__(self, row_num, name, mat):
+        self.row_num = row_num
+        self.name    = name
+        self.mat     = mat
+
+class locations(db.Model):
     name = sa.Column(sa.String, primary_key=True)
     lat  = sa.Column(sa.FLOAT)
     lng  = sa.Column(sa.FLOAT)
--- apps/recyclops/views.py
+++ apps/recyclops/views.py
@@ -1,9 +1,10 @@
+import logging
+
 from flask import Blueprint, request, Response
-from apps.recyclops.models import centers
+from apps.recyclops.models import centers, locations
 from DAO.database import db
 import json
 import csv
-
 
 """"
 Views file: 
@@ -19,54 +20,62 @@ are exposed here.
 
 recyclopsApp = Blueprint("recyclopsApp", __name__)
 
-
 """
 base route to serve HTML, CSS, JS, etc .... 
 Subject to change if frontend wants 
 to serve up static files from different 
 server and call API indirectly 
 """
-@recyclopsApp.route("/")
-def root():
-    pass
-
 
 """ 
 HTTP GET request to return list 
 of recycling centers for a given 
 material. 
 """
+
+
 @recyclopsApp.get("/getLocations")
 def getLocations():
     queryParams = request.args.to_dict()
 
     if "material" not in queryParams.keys():
+        print("ERROR, materials param not provided")
         return Response(json.dumps(constructError("Query param 'material' was not provided", 400)),
                         status=400,
                         mimetype="application/json")
 
     try:
+        print(f"Querying for: {queryParams['material']}")
         filtered = {"locations": []}
 
         # - perform the database query. Please search for 'sqlalchemy queries' for more info
         # - on how the queries operate.
-        #recyclingCenters = db.session.query(centers.center_name, centers.latitude, centers.longitude)\
-        #    .filter(centers.material == queryParams["material"]).all()
+        recyclingCenters = db.session.query(centers.name) \
+            .filter(centers.mat == queryParams["material"]).all()
 
-        recyclingCenters = []
-        for rc in recyclingCenters:
+        if len(recyclingCenters) == 0:
+            return Response(constructError(f"Could not find any centers for material: {queryParams['material']}", 400),
+                            status=400,
+                            mimetype="application/json")
+
+        loc = []
+        for center in recyclingCenters:
+            res = db.session.query(locations.name, locations.lat, locations.lng)\
+                .filter(locations.name == center[0]).one()
+
+            loc.append(res)
+
+        for rc in loc:
             filtered["locations"].append({"center name": rc[0], "latitude": rc[1], "longitude": rc[2]})
+            
+        res = Response(json.dumps(filtered), status=200, mimetype="application=/json")
+        return res
 
-        return Response(json.dumps(filtered),
-                        status=200,
-                        mimetype="application=/json")
-
-    except Exception:
-        """ write better error handling functionality """
+    except:
+        logging.exception('')
         return Response(json.dumps(constructError("Exception occurred during processing", 500)),
                         status=500,
                         mimetype="application/json")
-
 
 """ 
 HTTP post request to insert data into the 
@@ -74,14 +83,29 @@ HTTP post request to insert data into the
 all database data when recycling center compilation 
 is complete
 """
-@recyclopsApp.post("/insertTable")
-def createCenters():
-    with open("/home/phil/PycharmProjects/442/recyclops/location.csv") as f:
+
+
+@recyclopsApp.post("/insertLocations")
+def createLocations():
+    with open("location.csv") as f:
         reader = csv.reader(f)
         next(reader)
 
         for line in reader:
-            db.session.add(centers(line[0], float(line[1]), float(line[1])))
+            db.session.add(locations(line[0], float(line[1]), float(line[1])))
+            db.session.commit()
+
+    return Response(json.dumps({"message": "Insertions successful!"}), status=200, mimetype="application/json")
+
+
+@recyclopsApp.post("/insertCenters")
+def createCenters():
+    with open("/home/phil/PycharmProjects/442/recyclops/apps/recyclops/centers.csv") as f:
+        reader = csv.reader(f)
+        next(reader)
+
+        for line in reader:
+            db.session.add(centers(int(line[0]), line[1], line[2]))
             db.session.commit()
 
     return Response(json.dumps({"message": "Insertions successful!"}), status=200, mimetype="application/json")
@@ -90,5 +114,7 @@ def createCenters():
 """
 helper method to construct error messages
 """
+
+
 def constructError(errorMsg, errorCode):
-    return {"Error Message":  errorMsg, "error code": errorCode}
+    return {"Error Message": errorMsg, "error code": errorCode}
